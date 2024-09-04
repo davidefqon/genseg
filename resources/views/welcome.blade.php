ddd3fa7d67aa7466ac714b5ef2090e390079c047
davidefqon <davidlarotapilco@gmail.com>
2024-09-04 09:50:59 -0500

    mi nombre completo pruebas a git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,7 +163,7 @@
                     </main>
 
                     <div>
-                        David
+                        David brahyan larota pilco
                     </div>
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
