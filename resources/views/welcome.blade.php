2abb8b6d97c66c0ff4b6ce7baa33a2935f144f0b
ApazaRoss <roapazacc@gmail.com>
2024-09-06 09:03:56 -0500

    lenin problema solucionando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,7 +171,7 @@
                     <div>
                         rous
                     </div>
-                    <h1>Lenin :)</h1>
+                    <h1>Lenin </h1>
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </footer>
